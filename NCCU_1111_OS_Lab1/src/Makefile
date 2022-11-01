19ad6bddfebad29917f280e2a9493384cc5a1904
neo-lu15 <neo901115@gmail.com>
2022-11-01 09:18:27 +0000

    feature jiffies and running seconds

--- NCCU_1111_OS_Lab1/src/Makefile
+++ NCCU_1111_OS_Lab1/src/Makefile
@@ -1,4 +1,4 @@
-obj-m += simple.o
+obj-m += seconds.o
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
--- NCCU_1111_OS_Lab1/src/seconds.c
+++ NCCU_1111_OS_Lab1/src/seconds.c
@@ -44,7 +44,7 @@ static int proc_init(void)
     ////<在此加入程式碼>////
     /////////////////////
     //用變數記住載入模組時的jiffies
-    
+	init_jiffies = jiffies;    
     
         // 創出/proc/seconds這個項目
         proc_create(PROC_NAME, 0, NULL, &proc_ops);
@@ -78,14 +78,17 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
                 return 0;
         }
         completed = 1;
-
+	
         // 計算目前jiffies和模組初始jiffies的差，並除以HZ值
         // 將結果透過sprintf放到buffer中，使用%ld印出
-        rv = sprintf(buffer,
+        total_elapsed_time = (jiffies - init_jiffies) / HZ;
+
+	rv = sprintf(buffer,
                      "Module has been running for %ld seconds\n",
                      //////////////////////
                      ////<在此加入程式碼>////
                      /////////////////////
+		    total_elapsed_time
                      );
 
     
